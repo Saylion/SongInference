872dccd32772444d244dc12461be4fa5a476fbe2
Saylion <ilyasassahmi123@gmail.com>
2024-04-23 13:25:28 +0700

    Added some features

--- src/main.py
+++ src/main.py
@@ -353,7 +353,7 @@ def song_cover_pipeline(song_input, voice_model, backvoice_model, pitch_change, 
             ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_main_p{pitch_change}_i{index_rate}_fr{filter_radius}_rms{rms_mix_rate}_pro{protect}_{f0_method}{"" if f0_method != "mangio-crepe" else f"_{crepe_hop_length}"}.wav')
             ai_back_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{backvoice_model}_backup_p{backvoc_pitch_change}_i{index_rate}_fr{filter_radius}_rms{rms_mix_rate}_pro{protect}_{f0_method}{"" if f0_method != "mangio-crepe" else f"_{crepe_hop_length}"}.wav')
 
-        if not os.path.exists(ai_vocals_path, ai_back_vocals_path):
+        if not os.path.exists(ai_vocals_path) and not os.path.exists(ai_back_vocals_path):
             display_progress('[~] Converting voice using RVC...', 0.5, is_webui, progress)
             voice_change(voice_model, main_vocals_dereverb_path, ai_vocals_path, pitch_change, f0_method, index_rate, filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
             if backvoc_infer == True:
