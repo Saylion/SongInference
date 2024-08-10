78ce6c1734b22566e905c06bb5bcbca1981f28d5
Saylion <ilyasassahmi123@gmail.com>
2024-08-10 11:45:27 +0700

    Fixing BUGS

--- src/demucs.py
+++ src/demucs.py
@@ -4,17 +4,36 @@ import glob
 import subprocess
 import shutil
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def rename(orig_song_path):
+    filename = orig_song_path
+    if orig_song_path.startswith('/'):
+        split_orig_song_path = orig_song_path.split('/')
+        if split_orig_song_path[-1]:
+            file = split_orig_song_path[-1]
+            file = re.sub(r'[^a-zA-Z0-9.]', '_', file)
+            split_orig_song_path.pop()
+            filename = '/'.join(split_orig_song_path) + '/' + file
+            os.rename(orig_song_path, filename)
+    else:
+        filename = re.sub(r'[^a-zA-Z0-9.]', '_', orig_song_path)
+        search_file_mp3 = glob.glob("*.mp3")
+        for file in search_file_mp3:
+          new_name = filename
+
+          os.rename(file, new_name)
+
+    return filename
 
 def run_demucs(song_output_dir, orig_song_path, keep_orig):
-    filename = re.sub(r'[^a-zA-Z0-9.]', '_', orig_song_path)
-    search_file_mp3 = glob.glob("*.mp3")
-    for file in search_file_mp3:
-        new_name = filename
+    if orig_song_path.startswith('/') and not orig_song_path.startswith('/tmp/'):
+      orig_song_path = shutil.copy(orig_song_path, BASE_DIR)
 
-        os.rename(file, new_name)
+    filename = rename(orig_song_path)
 
     get_model = 'htdemucs'
-
+    print(filename)
     command = f'demucs --two-stems=vocals -n {get_model} --out {song_output_dir} {filename}'
 
     subprocess.run(command.split(), stdout=subprocess.PIPE)
@@ -43,10 +62,10 @@ def run_demucs(song_output_dir, orig_song_path, keep_orig):
     new_inst_name = os.path.join(sep_path, f'{os.path.basename(os.path.splitext(orig_song_path)[0])}_Instrumental.wav')
     os.rename(old_inst_name, new_inst_name)
     shutil.move(new_inst_name,song_output_dir)
-    
-
-    if not keep_orig:
-        os.remove(filename)
-        shutil.rmtree(f'{os.path.join(song_output_dir,get_model)}')
+    if not orig_song_path.startswith('/tmp/'):
+      os.remove(filename)
+    else:
+      os.rename(filename, orig_song_path)
+    shutil.rmtree(f'{os.path.join(song_output_dir,get_model)}')
 
     return vocal_path, inst_path
